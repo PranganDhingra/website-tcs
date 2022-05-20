3ffff6e19e2400d6784081162b670a57c2edd29e
Prangan Dhingra <73108761+PranganDhingra@users.noreply.github.com>
2022-05-20 20:50:40 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.7.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBurVr9kYNfB9QtWjFIeOlid3iHE8cHKD0",
+    authDomain: "website-71c99.firebaseapp.com",
+    projectId: "website-71c99",
+    storageBucket: "website-71c99.appspot.com",
+    messagingSenderId: "254370669587",
+    appId: "1:254370669587:web:9f101026d62756886e4be6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
